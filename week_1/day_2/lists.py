d5d77dbffcfe7dda42f4c527484f7ea1bac9218f
yosefshatila00 <ykshatela@hotmail.com>
2025-07-25 15:47:46 +0300

    just added files

---
+++ week_1/day_2/lists.py
@@ -0,0 +1,14 @@
+list1=[5, 10, 15,20, 25, 50,20]
+if 20 in list1:
+    index1=list1.index(20)
+    list1[index1]=200
+
+print(list1)
+
+
+a_tuple = (10, 20, 30, 40)
+a, b, c, d = a_tuple
+print(a)
+print(b)
+print(c)
+print(d)
